c4fb973e4c889e98643222a04d7dcaa99a861e02
mdsohag65 <sohag92ahmed@gmail.com>
2022-04-12 10:18:09 +0600

    login handle

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,7 +1,13 @@
 /**
- * 1. create a new project in console.google.com
+ * 1. create a new firebase project in console.google.com
  * 2. npm install firebase
  * 3. crreate firebase.init.js and import export auth
  * 4. Firebase settings > Authentication > enable Email and Password auth
- * 5. 
+ * 5. create login , signup component, set route
+ * 6. attach from field handler and submit handler
+ * 7. npm install --save react-firebase-hooks
+ * 8. useCreateUserWithEmailAndPassword from react firebse hooks
+ * 9. if user is created redicrect to the expected page 
+ * 10. useCreateUserWithEmailAndPassword for sign in
+
 */
